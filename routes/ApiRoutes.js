d14e7a1923708a93cfd363e2077c1b82caa5e732
ChrisOnions <c.onions101@gmail.com>
2021-05-07 16:55:08 +1000

    Fixed server to be able to see style.css. Bug fswrite file in api routes needs to be edited

--- routes/ApiRoutes.js
+++ routes/ApiRoutes.js
@@ -1,15 +1,25 @@
 const Data_base_JSON = require('../db/db.json')
+const fs = require('fs');
+const FilePath = '../db/db.json'
 
 module.exports = (app) => {
 //Api Get Request
-  app.get('api/notes', (req , res) => {
+  app.get('/api/notes', (req , res) => {
     res.json(Data_base_JSON)
   })
 
-app.post('api/notes',(req,res) => {
- Data_base_JSON.push(req.body) 
+app.post('/api/notes',(req,res) => {
+  console.log(res);
+//  Data_base_JSON.push(req.body) 
+fs.writeFile(FilePath, res.body ,(err) => {
+  if (err) console.log(err);
+  console.log('The file has been saved!');
+})
 })
 // require / Add fs 
 // Write file to db.json with unique name
 // require / Import unique name gen
+}
+function ADDdata (Database) {
+  
 }
